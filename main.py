22723a617007ad82fc2953f7c6b180bfc8231b5e
elasanchez01 <dasancheza@unal.edu.co>
2023-08-22 10:52:33 -0500

    update main

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ class Asiento:
             self.color = color
 
 class Auto():
-    def __init__(self, modelo, precio, asientos, marca, motor, registro, cantidadCreados):
+    cantidadCreados = 0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
         self.modelo = modelo
         self.precio = precio
         self.asientos = asientos
         self.marca = marca
         self.motor = motor
         self.registro = registro
-        self.cantidadCreados = cantidadCreados
 
     
